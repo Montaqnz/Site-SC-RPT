71ec169d7802cb7d4e795ee74b9436d7daa87c46
Montaqnz <furkanitmez44@gmail.com>
2025-08-14 02:15:39 +0300

    Create next.js

---
+++ next.js
@@ -0,0 +1,8 @@
+my-vercel-app/
+├── pages/
+│   ├── index.js          ← Form burada
+│   └── api/
+│       └── sorgu.js      ← MySQL işlemleri burada
+├── .env.local            ← Veritabanı bilgileri burada
+├── package.json
+├── ...
