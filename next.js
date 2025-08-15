c86504b92dc3ba3c1953850b91f6dad0cfb9eebf
Montaqnz <furkanitmez44@gmail.com>
2025-08-15 21:00:02 +0300

    Delete next.js

--- next.js
+++
@@ -1,8 +0,0 @@
-my-vercel-app/
-├── pages/
-│   ├── index.js          ← Form burada
-│   └── api/
-│       └── sorgu.js      ← MySQL işlemleri burada
-├── .env.local            ← Veritabanı bilgileri burada
-├── package.json
-├── ...
